657d47cafdcd0193d14c3d1ed90e8fe60c72e839
Barak Margalit <barakma@wipp-ml2.weizmann.ac.il>
2026-01-06 11:46:48 +0200

    odds

--- src/hepattn/experiments/odd/start.sh
+++ src/hepattn/experiments/odd/start.sh
@@ -1,1 +1,5 @@
-qsub -o output.log -e error.log -q N -N IndexCHange -l walltime=72:00:00,mem=24gb,ncpus=2,ngpus=1,io=1,gputype=A6000 brk.sh
+pushd /storage/agrp/barakma/hepattn/src/hepattn/experiments/odd
+
+qsub -o output.log -e error.log -q N -N IndexCHange -l walltime=72:00:00,mem=48gb,ncpus=2,ngpus=1,io=1,gputype=A6000 brk.sh
+
+popd
