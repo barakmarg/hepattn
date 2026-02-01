5ab8f7857fcb80c0f99c0fbb3472b3990bae416e
Barak Margalit <barakma@wipp-ml2.weizmann.ac.il>
2026-02-01 17:17:58 +0200

    great work

--- src/hepattn/experiments/odd/run_eval.sh
+++ src/hepattn/experiments/odd/run_eval.sh
@@ -2,11 +2,12 @@
 
 #odd_pflow_v1_clic_arch_20260114-T151040 - overtrainned, epoch=999-val_loss=1.05913.ckpt
 export COMET_API_KEY=rw9qVay7dAEGfWtM0hgakSmIh
-export M_ODD=/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd/logs/odd_pflow_v1_clic_arch_20260120-T111816
-export CKPT_PATH=${M_ODD}/ckpts/ # specify epoch and step
+export M_ODD=/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd/logs/odd_pflow_v1_clic_arch_20260128-T131927
+export CKPT_PATH=${M_ODD}/ckpts/epoch=119-val_loss=4.43505.ckpt
+ # specify epoch and step
 source /usr/wipp/conda/24.5.0u/bin/activate /usr/wipp/conda/24.5.0u/envs/common
 pushd /storage/agrp/barakma/hepattn/src/hepattn/experiments/odd
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-python main.py test --config configs/base.yaml --ckpt_path $CKPT_PATH --data.num_workers 0
+python main.py test --config configs/base.yaml --ckpt_path $CKPT_PATH 
 popd
 #/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd/logs/odd_pflow_v1_clic_arch_20260112-T143417/ckpts/epoch=199-val_loss=4.48795__test.h5
