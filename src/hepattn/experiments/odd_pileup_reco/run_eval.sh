149f1f5969738a4027f3ad8da8ad58994c02e91d
Barak Margalit <barakma@wipp-ml2.weizmann.ac.il>
2026-05-03 14:53:02 +0300

    before new experiment

--- src/hepattn/experiments/odd_pileup_reco/run_eval.sh
+++ src/hepattn/experiments/odd_pileup_reco/run_eval.sh
@@ -1,9 +1,9 @@
 # run_eval.sh
 
-#odd_pflow_v1_clic_arch_20260114-T151040 - overtrainned, epoch=999-val_loss=1.05913.ckpt
+#/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd_pileup_reco/logs/odd_pflow_reco_20260421-T104425/ckpts/epoch=099-val_loss=13.99981.ckpt
 export COMET_API_KEY=rw9qVay7dAEGfWtM0hgakSmIh
-export M_ODD=/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd_pileup_reco/logs/odd_pflow_reco_20260412-T163313
-export CKPT_PATH=${M_ODD}/ckpts/epoch=028-val_loss=10.78560.ckpt
+export M_ODD=/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd_pileup_reco/logs/odd_pflow_reco_20260421-T104425
+export CKPT_PATH=${M_ODD}/ckpts/epoch=099-val_loss=13.99981.ckpt
  # specify epoch and step
 source /usr/wipp/conda/24.5.0u/bin/activate /usr/wipp/conda/24.5.0u/envs/common
 pushd /storage/agrp/barakma/hepattn/src/hepattn/experiments/odd_pileup_reco
@@ -12,4 +12,4 @@ export IOTHROTTLE_LIMIT=100
 
 python main.py test --config configs/base.yaml --ckpt_path $CKPT_PATH 
 popd
-#/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd_pileup_reco/logs/odd_pflow_reco_20260412-T163313/ckpts/epoch=028-val_loss=10.78560.ckpt
+#/storage/agrp/barakma/hepattn/src/hepattn/experiments/odd_pileup_reco/logs/odd_pflow_reco_20260421-T104425/ckpts/epoch=099-val_loss=13.99981.ckpt
